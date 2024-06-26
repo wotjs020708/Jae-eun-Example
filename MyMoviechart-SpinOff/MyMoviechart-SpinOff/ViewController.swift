4e57a01b0352bf911b3a8ecd38b72ee8f149bc5e
JaeSunEo <wotjs020708@gmail.com>
2024-06-26 23:30:15 +0900

    MyMoviechart-SpinOff 실습

--- MyMoviechart-SpinOff/MyMoviechart-SpinOff/ViewController.swift
+++ MyMoviechart-SpinOff/MyMoviechart-SpinOff/ViewController.swift
@@ -17,3 +17,26 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5 // 임의로 5개 셀 생성
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        var content = cell.defaultContentConfiguration()
+        content.text = "\(indexPath.row)번쨰 데이터 입니다."
+        cell.contentConfiguration = content
+        return cell
+        
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NSLog("\(indexPath.row)번 쨰 데이터가 클릭됨")
+    }
+}
+
