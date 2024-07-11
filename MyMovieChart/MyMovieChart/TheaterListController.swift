62d1b13e451f7d71ffdcbaef6566909711e00ecb
JaeSunEo <wotjs020708@gmail.com>
2024-07-11 16:09:05 +0900

    Mapkit 추가

--- MyMovieChart/MyMovieChart/TheaterListController.swift
+++ MyMovieChart/MyMovieChart/TheaterListController.swift
@@ -85,6 +85,19 @@ class TheaterListController: UITableViewController {
         
         return cell
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "segue_map") { // 실행된 세그웨이의 식별자 segue_map이면
+            // 선택된 셀의 행 정보
+            let path = self.tableView.indexPath(for: sender as! UITableViewCell)
+            // 선태된 셀에 사용된 데이터
+            let data = self.list[path!.row]
+            // 세그웨이가 이동할 목적지 뷰 컨트롤러 객체를 구하고 선언된 param 변수에 데이터를 연결해준다.
+            (segue.destination as? TheaterViewController)?.param = data
+        }
+        
+    }
+
 }
 
 
